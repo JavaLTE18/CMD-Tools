71243852ee0aebd2af70cdc38b2f903c1e2cdfc4
JabaLTE172 <deeznutsmc.xyz@gmail.com>
2022-07-09 15:49:39 +0800

    Create Enable-Control_Panel-GodMode.bat

---
+++ Enable-Control_Panel-GodMode.bat
@@ -0,0 +1,4 @@
+echo Creating GodMode Folder On Desktop
+mkdir %userprofile%/Desktop/GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}
+echo Created Folder Named : GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}
+echo Tips : The GodMode Control Panel Will Be Useful In Windows 11 Because Microsoft Removed a lot of tools.
